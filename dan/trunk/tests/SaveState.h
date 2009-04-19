d74e3f4a440f62b002494d777a49f9cfe542d718
alan grover <alan.grover@bbb89a9a-b62b-0410-a984-61d6f13158f5>
2009-04-19 02:13:29 +0000

    Added MSVC macros to save and restore state; replacements for Duff's Device

---
+++ dan/trunk/tests/SaveState.h
@@ -0,0 +1,41 @@
+#ifndef SAVE_STATE_H_
+#define SAVE_STATE_H_
+
+// saveState saves the current state (instruction pointer)
+// state - integer to hold the state
+// label - a label name that must be unique in the scope
+// Rationale: can't use goto because it doesn't work with
+// a saved state. setjmp/longjmp is overkill; we don't need
+// a stack context and aren't doing a non-local jump.
+// Duff's Device may work for simple cases, but becomes
+// unweildy rapidly and overly complicates code generation.
+#define saveState(state, label)				        \
+	do                                              \
+	{										        \
+		unsigned int tstate = 0xdeadbeef;	        \
+		__asm								        \
+		{									        \
+			__asm mov eax, offset label	            \
+			__asm mov tstate, eax			        \
+		}									        \
+		state = tstate;						        \
+label:                                              \	
+        /* bug? MSVC Express 2008 freaks out        \
+        if the last statement has a semi-colon */   \
+		__asm xor eax, eax					        \
+	} while (0)								
+
+// restoreState jumps to the state saved
+// in saveState
+#define restoreState(state)					\
+	do                                      \
+    {									    \
+		unsigned int tstate = state;		\
+        if(tstate != 0)                     \
+		    __asm jmp tstate       			\
+	} while (0)								
+
+// TODO need a version for GCC 
+// TODO possibly should make these take uint32* instead of uint32
+
+#endif
