046c5c0133dff92934702b53c3bae878b23b83a1
alan grover <alan.grover@bbb89a9a-b62b-0410-a984-61d6f13158f5>
2009-04-30 04:11:45 +0000

    added GNU version of saveState & restoreState

--- dan/trunk/tests/SaveState.h
+++ dan/trunk/tests/SaveState.h
@@ -1,6 +1,8 @@
 #ifndef SAVE_STATE_H_
 #define SAVE_STATE_H_
 
+// TODO possibly should make these take uint32* instead of uint32
+
 // saveState saves the current state (instruction pointer)
 // state - integer to hold the state
 // label - a label name that must be unique in the scope
@@ -9,6 +11,7 @@
 // a stack context and aren't doing a non-local jump.
 // Duff's Device may work for simple cases, but becomes
 // unweildy rapidly and overly complicates code generation.
+#ifdef _MSC_VER
 #define saveState(state, label)				        \
 	do                                              \
 	{										        \
@@ -23,10 +26,28 @@ label:                                              \
         /* bug? MSVC Express 2008 freaks out        \
         if the last statement has a semi-colon */   \
 		__asm xor eax, eax					        \
-	} while (0)								
+	} while (0)
+
+#elif __GNUC__
+#ifdef __i386__
+#define saveState(state, label)                             \
+    do                                                      \
+    {                                                       \
+        unsigned int tstate = 0xdeadbeef;                   \
+        state = tstate;                                     \
+        __asm__ __volatile__("movl $label, %0; label:"      \
+            : "=m" (state)                                  \
+        );                                                  \
+    } while (0)        
+#endif // __i386__
+
+#else // unsupported compiler
+#barf: unsupported compiler
+#endif // compiler brand
 
 // restoreState jumps to the state saved
 // in saveState
+#ifdef _MSC_VER
 #define restoreState(state)					\
 	do                                      \
     {									    \
@@ -35,7 +56,21 @@ label:                                              \
 		    __asm jmp tstate       			\
 	} while (0)								
 
-// TODO need a version for GCC 
-// TODO possibly should make these take uint32* instead of uint32
+#elif __GNUC__
+#ifdef __i386__
+// TODO The GNU version chucks a warning: indirect jmp without `*'
+// but the program seems to work fine.  Not sure yet how to placate it
+#define restoreState(state)                                         \
+    do                                                              \
+    {                                                               \
+        unsigned int tstate = state;                                \
+        __asm__ __volatile__("jmp %0" : : "m" ((void*) state));     \
+    } while (0)                
+#endif // __i386__
 
-#endif
+#else // unsupported compiler
+#barf: unsupported compiler
+#endif // compiler brand
+
+#endif // SAVE_STATE_H_
+
