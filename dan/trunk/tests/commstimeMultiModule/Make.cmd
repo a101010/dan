c16c97b0ea594d85154274beddf2194282a03211
alan grover <alan.grover@bbb89a9a-b62b-0410-a984-61d6f13158f5>
2011-04-01 20:23:04 +0000

    Added 3 tests

---
+++ dan/trunk/tests/commstimeMultiModule/Make.cmd
@@ -0,0 +1,6 @@
+java -jar "..\..\front_end\dist\front_end.jar" BInt.dan
+java -jar "..\..\front_end\dist\front_end.jar" Prefix.dan
+java -jar "..\..\front_end\dist\front_end.jar" Succ.dan
+java -jar "..\..\front_end\dist\front_end.jar" Delta.dan
+java -jar "..\..\front_end\dist\front_end.jar" Count.dan
+java -jar "..\..\front_end\dist\front_end.jar" Commstime.dan
